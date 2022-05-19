5987b080a83b6a68cfa06e398d365f7264ac6504
Catherine Kawara <ndutakawara2@gmail.com>
2022-05-19 10:40:35 +0300

    commit tree 7e3311c2b6d82d4d1c6d4ee77139101f1f984246

--- src/components/QuestionItem.js
+++ src/components/QuestionItem.js
@@ -1,23 +1,31 @@
 import React from "react";
 
-function QuestionItem({ question }) {
+function QuestionItem({ question, onAnswerChange,onDelete }) {
   const { id, prompt, answers, correctIndex } = question;
 
   const options = answers.map((answer, index) => (
     <option key={index} value={index}>
       {answer}
     </option>
-  ));
+  )); 
+
+  function handleChange(e){
+    onAnswerChange(id, parseInt(e.target.value))
+  }
+
+  function handleDelete(){
+    onDelete(id)
+  }
 
   return (
     <li>
-      <h4>Question {id}</h4>
+      <h4>Question {id}</h4> 
       <h5>Prompt: {prompt}</h5>
       <label>
         Correct Answer:
-        <select defaultValue={correctIndex}>{options}</select>
+        <select defaultValue={correctIndex} onChange={handleChange}>{options}</select>
       </label>
-      <button>Delete Question</button>
+      <button onClick={handleDelete}>Delete Question</button>
     </li>
   );
 }
